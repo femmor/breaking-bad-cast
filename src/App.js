f8e10d7542cf9b86136bfb9eb518ffd8d1d6e7d1
Emmanuel Egomson <fegomson@gmail.com>
2020-12-09 13:10:59 -0600

    fetch data from base url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import {useState, useEffect} from "react"
+import axios from 'axios'
 import './App.css';
 
+import Header from "./components/ui/Header"
+
 const App = () => {
+
+  // UseState
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  // UseEffects
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
+    }
+    // call fetchItems
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
-      Hello
+        <Header/>
     </div>
   );
 }
---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from "../../img/logo.png"
+
+const Header = () => {
+    return (
+        <>
+            <header className="center">
+                <img src={Logo} alt="app-logo"/>
+            </header>
+        </>
+    )
+}
+
+export default Header
